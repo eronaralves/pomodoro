ad13e94bc7292fe2b3fb84f1601393a020c4382e
Eronar Alves <alveseronar@gmail.com>
2022-12-02 00:43:25 -0300

    adding useReducer in the project

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,7 @@
-import { createContext, ReactNode, useState, useReducer } from 'react'
-import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles';
+import { differenceInSeconds } from 'date-fns';
+import { createContext, ReactNode, useState, useReducer, useEffect } from 'react'
+import { ActionTypes, addNewCycleAction, interruptCurrentCycleAction, markCurrentAsFinishedAction } from '../reducers/cycles/actions';
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer';
 
 interface CreateCycleDate {
   task: string;
@@ -28,20 +30,33 @@ export function CyclesContenxtProvider({children}: CyclesContenxtProvider) {
   const [cyclesState, dispatch] = useReducer(cyclesReducer, {
     cycles: [],
     activeCycleId: null
+  }, () => {
+    const storangeStateAsJSON = localStorage.getItem('@pomodoro:cycles-state.1.0.0')
+
+    if(storangeStateAsJSON) {
+      return JSON.parse(storangeStateAsJSON)
+    }
   })
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
   const {cycles, activeCycleId} = cyclesState;
-
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if(activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@pomodoro:cycles-state.1.0.0', stateJSON)
+  }, [cyclesState])
+
+
   function markCurrentCycleFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      }
-    })
+    dispatch(markCurrentAsFinishedAction())
   }
   
   function setSecondsPassed(seconds: number) {
@@ -58,22 +73,12 @@ export function CyclesContenxtProvider({children}: CyclesContenxtProvider) {
       startDate: new Date(),
     }
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    })
+    dispatch(addNewCycleAction(newCycle))
     setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      }
-    })
+    dispatch(interruptCurrentCycleAction())
   }
 
   return (
